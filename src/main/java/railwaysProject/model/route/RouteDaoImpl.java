fc2a9f4dd7d70e36f6418d260feeccd624fce1cf
Saddam <saddam.asmatullayev@nu.edu.kz>
2019-10-31 02:01:13 +0600

    refactored with new database

--- src/main/java/railwaysProject/model/route/RouteDaoImpl.java
+++ src/main/java/railwaysProject/model/route/RouteDaoImpl.java
@@ -19,20 +19,20 @@ public class RouteDaoImpl implements RouteDAO {
     public List<Route> getRoutesFromTo(int stationFrom, int stationTo, String date) {
 
 
-        String sqlQuery =   "SELECT Route.route_id, Route.start_date, Route.route_name, Arrival.date as arr_date, " +
+        String sqlQuery =   "SELECT Route.route_id, Route.start_date, R.route_name, Arrival.date as arr_date, " +
                             "Arrival.station_id as to_station, Departure.date as dep_date, Departure.station_id as from_station, " +
                             "stto.station_name as to_name, stfrom.station_name as from_name " +
-                            "FROM ((Route INNER JOIN Arrival ON Arrival.route_id = Route.route_id " +
+                            "FROM ((Route_Instance as Route INNER JOIN Arrival ON Arrival.route_id = Route.route_id " +
                             "AND Route.start_date = Arrival.route_start_date) INNER JOIN Departure " +
-                            "ON Departure.route_id = Route.route_id AND Departure.route_start_date = Route.start_date), station stto, " +
+                            "ON Departure.route_id = Route.route_id AND Departure.route_start_date = Route.start_date), Route R, station stto, " +
                             "station stfrom\n" +
-                            "WHERE start_date = " + date + " AND stto.station_id = Arrival.station_id AND stfrom.station_id = Departure.station_id " +
+                            "WHERE R.route_id = Route.route_id AND start_date = " + date + " AND stto.station_id = Arrival.station_id AND stfrom.station_id = Departure.station_id " +
                             "AND Arrival.station_id IN (SELECT station_id FROM station WHERE locality_locality_id = " + stationTo + ") " +
                             "AND Departure.station_id IN (SELECT station_id FROM station WHERE locality_locality_id = " + stationFrom +");";
 
-        String newSqlQuery =    "SELECT Route.route_id, Route.start_date, Route.route_name, Arrival.date as arr_date, " +
+        String newSqlQuery =    "SELECT Route.route_id, Route.start_date, Arrival.date as arr_date, " +
                                 "Arrival.station_id as to_station, Departure.date as dep_date, Departure.station_id as from_station " +
-                                "FROM ((Route INNER JOIN Arrival ON Arrival.route_id = Route.route_id " +
+                                "FROM ((Route_Instance as Route INNER JOIN Arrival ON Arrival.route_id = Route.route_id " +
                                 "AND Route.start_date = Arrival.route_start_date) INNER JOIN Departure " +
                                 "ON Departure.route_id = Route.route_id AND Departure.route_start_date = Route.start_date) " +
                                 "WHERE start_date = " + date + " " +
@@ -74,10 +74,10 @@ public class RouteDaoImpl implements RouteDAO {
     public List<CityRoute> getArrivalToTheCity(int cityId) {
 
         String newSqlQuery =    "SELECT " +
-                                "Route.route_id, Route.start_date, Route.route_name, Arrival.station_id, Arrival.date as arr_date, station.station_name " +
-                                "FROM ((Route INNER JOIN Arrival ON Arrival.route_id = Route.route_id " +
+                                "Route.route_id, Route.start_date, R.route_name,  Arrival.station_id, Arrival.date as arr_date, station.station_name " +
+                                "FROM Route R, ((Route_Instance Route INNER JOIN Arrival ON Arrival.route_id = Route.route_id " +
                                 "AND Route.start_date = Arrival.route_start_date) INNER JOIN station on station.station_id = arrival.station_id) " +
-                                "WHERE Arrival.station_id in (SELECT station_id FROM station WHERE locality_locality_id = " + cityId + ");";
+                                "WHERE R.route_id = Route.route_id AND Arrival.station_id in (SELECT station_id FROM station WHERE locality_locality_id = " + cityId + ");";
 
         Connection conn;
         ArrayList<CityRoute> routes = new ArrayList<>();
@@ -87,8 +87,8 @@ public class RouteDaoImpl implements RouteDAO {
             while(result.next()) {
                 int routeId = result.getInt("route_id");
                 String startDate = result.getString("start_date");
-                String routeName = result.getString("route_name");
                 int stationId = result.getInt("station_id");
+                String routeName = result.getString("route_name");
                 String stationName = result.getString("station_name");
                 String arrDate = result.getString("arr_date");
 //                System.out.println("routeId: " + routeId + ", route_name: " + routeName + "to: " + to);
@@ -109,9 +109,9 @@ public class RouteDaoImpl implements RouteDAO {
     @Override
     public List<CityRoute> getDepartureFromTheCity(int cityId) {
         String newSqlQuery =    "SELECT " +
-                "Route.route_id, Route.start_date, Route.route_name, Departure.station_id, Departure.date as dep_date, station.station_name " +
-                "FROM ((Route INNER JOIN Departure ON Departure.route_id = Route.route_id " +
-                "AND Route.start_date = Departure.route_start_date) INNER JOIN station on station.station_id = Departure.station_id) " +
+                "R.route_name, Route.route_id, Route.start_date, Departure.station_id, Departure.date as dep_date, station.station_name " +
+                "FROM Route R, ((Route_Instance Route INNER JOIN Departure ON Departure.route_id = Route.route_id " +
+                "AND R.route_id = Route.route_id AND Route.start_date = Departure.route_start_date) INNER JOIN station on station.station_id = Departure.station_id) " +
                 "WHERE Departure.station_id in (SELECT station_id FROM station WHERE locality_locality_id = " + cityId + ");";
 
         Connection conn;
@@ -121,8 +121,8 @@ public class RouteDaoImpl implements RouteDAO {
             ResultSet result = conn.createStatement().executeQuery(newSqlQuery);
             while(result.next()) {
                 int routeId = result.getInt("route_id");
-                String startDate = result.getString("start_date");
                 String routeName = result.getString("route_name");
+                String startDate = result.getString("start_date");
                 int stationId = result.getInt("station_id");
                 String stationName = result.getString("station_name");
                 String depDate = result.getString("dep_date");
